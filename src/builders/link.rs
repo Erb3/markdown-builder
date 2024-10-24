c2eb3f52f43f37e8a5360b267b8560660280500f
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-10-24 20:43:46 +0200

    fix: link builder wrong field order

--- src/builders/link.rs
+++ src/builders/link.rs
@@ -23,7 +23,12 @@ impl LinkBuilder {
         self
     }
 
-    pub fn footer(mut self, value: bool) -> Self {
+    pub fn footer(mut self) -> Self {
+        self.footer = true;
+        self
+    }
+
+    pub fn set_footer(mut self, value: bool) -> Self {
         self.footer = value;
         self
     }
@@ -33,13 +38,116 @@ impl LinkBuilder {
         self
     }
 
+    pub fn set_inlined(mut self, value: bool) -> Self {
+        self.inlined = value;
+        self
+    }
+
     pub fn build(self) -> Link {
-        Link::from(self.text, self.url, self.footer, self.inlined)
+        Link::from(self.url, self.text, self.footer, self.inlined)
     }
 }
 
 impl Link {
     pub fn builder() -> LinkBuilder {
         LinkBuilder::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn text_link_builder_text_url() {
+        let link = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .build();
+
+        assert_eq!(link.url, "https://www.rust-lang.org/");
+        assert_eq!(link.text, "A cool website");
+        assert_eq!(link.footer, false);
+        assert_eq!(link.inlined, false);
+    }
+
+    #[test]
+    fn text_link_builder_text_url_footer() {
+        let link = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .footer()
+            .build();
+
+        assert_eq!(link.url, "https://www.rust-lang.org/");
+        assert_eq!(link.text, "A cool website");
+        assert_eq!(link.footer, true);
+        assert_eq!(link.inlined, false);
+    }
+
+    #[test]
+    fn text_link_builder_text_url_set_footer() {
+        let link = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .set_footer(true)
+            .build();
+
+        assert_eq!(link.url, "https://www.rust-lang.org/");
+        assert_eq!(link.text, "A cool website");
+        assert_eq!(link.footer, true);
+        assert_eq!(link.inlined, false);
+
+        let link_footerless = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .set_footer(false)
+            .build();
+
+        assert_eq!(link_footerless.url, "https://www.rust-lang.org/");
+        assert_eq!(link_footerless.text, "A cool website");
+        assert_eq!(link_footerless.footer, false);
+        assert_eq!(link_footerless.inlined, false);
+    }
+
+    #[test]
+    fn text_link_builder_text_url_inlined() {
+        let link = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .inlined()
+            .build();
+
+        assert_eq!(link.url, "https://www.rust-lang.org/");
+        assert_eq!(link.text, "A cool website");
+        assert_eq!(link.footer, false);
+        assert_eq!(link.inlined, true);
+    }
+
+    #[test]
+    fn text_link_builder_text_url_set_inlined() {
+        let link_inlined = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .set_footer(true)
+            .set_inlined(true)
+            .build();
+
+        assert_eq!(link_inlined.url, "https://www.rust-lang.org/");
+        assert_eq!(link_inlined.text, "A cool website");
+        assert_eq!(link_inlined.footer, true);
+        assert_eq!(link_inlined.inlined, true);
+
+        let link = Link::builder()
+            .url("https://www.rust-lang.org/")
+            .text("A cool website")
+            .set_footer(false)
+            .set_inlined(false)
+            .build();
+
+        assert_eq!(link.url, "https://www.rust-lang.org/");
+        assert_eq!(link.text, "A cool website");
+        assert_eq!(link.footer, false);
+        assert_eq!(link.inlined, false);
     }
 }
